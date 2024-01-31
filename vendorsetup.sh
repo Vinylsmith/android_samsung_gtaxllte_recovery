c0538072544e67c18490a68e74188c1e6f07d8fe
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-01-31 10:12:56 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,53 +19,52 @@
 #
 FDEVICE="gtaxllte"
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
-   if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
+# gtaxllte specific
+export TARGET_DEVICE_ALT="gtaxl, gtaxllte, gtaxlltexx"
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
+# R11
+export FOX_R11="1"
+export FOX_BUILD_TYPE="Stable"
+export FOX_VERSION="R11.1"
+export FOX_ADVANCED_SECURITY="1"
+export OF_USE_TWRP_SAR_DETECT="1"
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
+export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;"
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
- export FOX_USE_NANO_EDITOR=1
- export FOX_ENABLE_APP_MANAGER=1
- export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-    export TW_DEFAULT_LANGUAGE="en"
- export LC_ALL="C"
-  export ALLOW_MISSING_DEPENDENCIES=true
- export TARGET_DEVICE_ALT="gtaxllte"
- export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY"
+# Common
+export TARGET_ARCH="arm64"
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL="C"
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY"
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/SYSTEM"
- export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
- export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
- export OF_NO_MIUI_PATCH_WARNING=1
- export FOX_USE_BASH_SHELL=1
- export FOX_ASH_IS_BASH=1
-  export FOX_DRASTIC_SIZE_REDUCTION=1
-  export FOX_REMOVE_AAPT=1
-  export FOX_REMOVE_ZIP_BINARY=1
-  export FOX_REMOVE_BASH=1
-  export BUILD_2GB_VERSION=1
 
- # let's see what are our build VARs
- if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-      export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-      export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-       export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-      export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-  fi
-else
- if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-  echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
- fi
-fi
-#
+export FOX_ASH_IS_BASH="1"
+export FOX_REPLACE_BUSYBOX_PS="1"
+export FOX_USE_BASH_SHELL="1"
+export FOX_USE_LZMA_COMPRESSION="1"
+export FOX_USE_NANO_EDITOR="1"
+export FOX_USE_TAR_BINARY="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export FOX_USE_ZIP_BINARY="1"
+export LZMA_RAMDISK_TARGETS="recovery"
+export OF_AB_DEVICE="0"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export OF_FLASHLIGHT_ENABLE="0"
+export OF_KEEP_FORCED_ENCRYPTION="1"
+export OF_MAINTAINER="SeifHecker2099"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_SCREEN_H="1920"
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+export OF_TWRP_COMPATIBILITY_MODE="1"
+export OF_USE_MAGISKBOOT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_USE_NEW_MAGISKBOOT="1"
+export PLATFORM_SECURITY_PATCH="2099-12-31"
+export PLATFORM_VERSION="16.1.0"
+export TW_DEFAULT_LANGUAGE="en"
+
+# use system (ROM) fingerprint where available
+export OF_USE_SYSTEM_FINGERPRINT="1"
 
 #add_lunch_combo omni_gtaxllte-eng
