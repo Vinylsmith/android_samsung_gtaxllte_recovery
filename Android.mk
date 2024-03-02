2ccbf7461fc53ee98c68a6dcb2da2dad4e931972
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-03 01:21:25 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,8 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifneq ($(filter gtaxllte, $(TARGET_DEVICE)),)
+
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),gtaxllte)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
