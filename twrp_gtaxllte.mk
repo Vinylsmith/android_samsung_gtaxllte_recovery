642bfda2fc131d3a235e4680c60ffa3f5d5be178
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-03 01:31:30 +0200

    Update twrp_gtaxllte.mk

--- twrp_gtaxllte.mk
+++ twrp_gtaxllte.mk
@@ -7,8 +7,9 @@
 
 PRODUCT_RELEASE_NAME := gtaxllte
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from twrp product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
