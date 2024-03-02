afced9ce2535256f558c9140bd91c702ca98f63e
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-03 00:58:33 +0200

    Update and rename omni_gtaxllte.mk to twrp_gtaxllte.mk

---
+++ twrp_gtaxllte.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_RELEASE_NAME := gtaxllte
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from twrp product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from this device
+$(call inherit-product, device/samsung/gtaxllte/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gtaxllte
+PRODUCT_NAME := twrp_gtaxllte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T585
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
